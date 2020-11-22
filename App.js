3032c7566d6ae4384cfbeebade8b9403560d73a4
won-joon <seowj819@gmail.com>
2020-11-22 18:40:54 +0900

    setup

--- App.js
+++ App.js
@@ -3,41 +3,45 @@ import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import axios from 'axios';
+import Weather from './Weather';
+
+const API_KEY = '5d3c0ed4bb41616c6d85da82944866b2';
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [datas, setData] = useState(null);
+  const [condition, setCondition] = useState("Clouds");
 
+  const getWeather = async(latitude, longitude) => {
+    const {data: {main: {temp}, weather}} = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    setLoading(false);
+    setData(temp);
+    setCondition(weather[0].main);
+  }
+  
   useEffect(() => {
     (async ()=> {
-      let {status} = await Location.requestPermissionsAsync();
-      if(status !== 'granted'){
-        setErrorMsg('Permission to access location was denied');
+      try{
+        let {status} = await Location.requestPermissionsAsync();
+        if(status !== 'granted'){
+          setErrorMsg('Permission to access location was denied');
+        }
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+        getWeather(location.coords.latitude, location.coords.longitude);
+      }catch(error){
+        console.log(error);
       }
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })
-  });
-
-  let text = 'Waiting...';
-  if(errorMsg){
-    text = errorMsg;
-  }else if(location){
-    text = JSON.stringify(location.coords);
-    console.log(text);
-  }
+    })();
+  },[]);
 
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <Loading />
-    </View>
+    isLoading ? <Loading /> : <Weather temp={Math.round(datas)} condition={condition}/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex : 1,
-  },
-});
 
---
+++ Weather.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import PropTypes from 'prop-types';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Ionicons } from '@expo/vector-icons';
+
+const WeatherOptions = {
+    Clouds: {
+        iconName: "ios-cloud",
+        gradient: ["#4DA0B0","#D39D38"]
+    },
+    Snow: {
+        iconName: "ios-snow",
+        gradient: ["#4DA0B0","#D39D38"]
+    }
+};
+
+export default function Weather({temp, condition}){
+    return (
+        <LinearGradient
+        colors={WeatherOptions[condition].gradient}
+        style={styles.container}>
+            <StatusBar barStyle="light-content" />
+            <View style={styles.halfcontainer}>
+                <Ionicons name={WeatherOptions[condition].iconName} size={100} color="white" />
+                <Text style={styles.textTemp}>{temp}°C</Text>
+                <Text>{condition}</Text>
+            </View>
+            <View style={styles.halfcontainer}>
+                <Text>TITLE</Text>
+                <Text>SUBTITLE</Text>
+            </View>
+        </LinearGradient>
+    );
+}
+
+Weather.propTypes = {
+    temp: PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Mist",
+        "Smoke",
+        "Haze",
+        "Dust",
+        "Ash",
+        "Squall",
+        "Tornado",
+        "Clear",
+        "Clouds"
+    ]).isRequired
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textTemp:{
+        fontSize: 42,
+        color: 'white',
+    },
+    halfcontainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
